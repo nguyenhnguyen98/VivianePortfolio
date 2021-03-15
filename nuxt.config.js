2008e965bdbc7d315b8af3cbbf50da41e6faee31
nguyen.h.nguyen <nguyen@timecodelab.com>
2021-03-15 18:39:12 -0400

    Fix title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio',
+    title: 'Viviane Tran Le - Portfolio',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Viviane Tran Le portfolio - Illustration student at Dawson College - 2021' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
